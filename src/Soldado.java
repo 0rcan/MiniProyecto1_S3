7bb41df5dcc0878dd8e13eb63618977887f3ca54
0rca <josue.zambrano@correounivalle.edu.co>
2024-11-02 15:34:27 -0500

    Se creo un menu principal

---
+++ src/Soldado.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+public class Soldado{ //Clase Principal Soldado 
+    
+    /////////////
+    //ATRIBUTOS//
+    /////////////
+
+    String nombre;
+    String id;
+    String rango;
+    
+    ///////////////
+    //CONSTRUCTOR//
+    ///////////////
+
+    public Soldado(String nombre, String id, String rango) {
+        this.nombre = nombre;
+        this.id = id;
+        this.rango = rango;
+    }
+    
+    public Soldado() {
+        
+    }
+}
