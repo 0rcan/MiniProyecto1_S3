1845d586396bb04277501cedc3ec3e258bd326ba
diego-11 <diego.isiquita@correounivalle.edu.co>
2024-11-02 16:43:24 -0500

    Cree las listas para las acciones que tienen el coronel

--- src/Coronel.java
+++ src/Coronel.java
@@ -1,3 +1,13 @@
-public class Coronel {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coronel extends Rango implements OperacionesMilitares{
+    //Clase coronel que hereda de Rango e implemebta la interfaz OperacionesMilitares
+
+    String estrategia;
+    static ArrayList<Coronel> listaCoronel= new ArrayList<>();
+    static List<String> accionCoronel=new ArrayList<>();
+    static List<String> misionCoronel=new ArrayList<>();
+    static List<String> reportarCoronel=new ArrayList<>();
     
 }
