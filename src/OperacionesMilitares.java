dcda25650bcb88b84e0a7694853fba2e66cb07cc
diego-11 <diego.isiquita@correounivalle.edu.co>
2024-11-02 16:29:04 -0500

    se hizo el cambio de clase a interfaz

--- src/OperacionesMilitares.java
+++ src/OperacionesMilitares.java
@@ -1,3 +1,16 @@
-public class OperacionesMilitares {
+import java.util.ArrayList;
+public interface OperacionesMilitares {
+     //Interfaces con las acciones del menu
+
+     public void crearSoldado();
+     public void mostrarInformacion();
+     public void modificarSoladado();
+
+     public void asignarMisison(String mision);
+     public void reportarEstado();
+     
+     // comentario
+     
+
     
 }
